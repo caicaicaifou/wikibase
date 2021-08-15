bc6d7af5218e88872ac2c386bbab9b8a30a5cc33
Always_Aki <always_aki@163.com>
2021-08-15 23:40:21 +0800

    0815 接口测试

---
+++ src/main/java/com/exec/wiki/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.exec.wiki.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Aki
+ */
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post, " + name;
+    }
+}
