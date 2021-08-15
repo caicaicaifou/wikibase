0b5e9a05668ba31ffffe2f0e6670306ddc58abce
Always_Aki <always_aki@163.com>
2021-08-15 22:40:06 +0800

    0815 启动日志

--- src/main/java/com/exec/wiki/WikiApplication.java
+++ src/main/java/com/exec/wiki/WikiApplication.java
@@ -1,13 +1,16 @@
 package com.exec.wiki;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class WikiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WikiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        log.info("wiki init start");
+        SpringApplication.run(WikiApplication.class, args);
+        log.info("wiki init success");
+    }
 }
