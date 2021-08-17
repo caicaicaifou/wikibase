041fe4b9ea7735a44ada03cbd1cd7328f1e8be3c
Always_Aki <always_aki@163.com>
2021-08-18 00:00:41 +0800

    0818 liquibase适配

---
+++ src/main/resources/liquibase/changelog/changelog-1.1.sql
@@ -0,0 +1,18 @@
+--liquibase formatted sql
+--changeset Aki:1
+
+DROP TABLE
+IF
+    EXISTS `ebook`;
+CREATE TABLE `ebook` (
+    `id` BIGINT NOT NULL COMMENT 'id',
+    `name` VARCHAR ( 50 ) COMMENT '名称',
+    `category1_id` BIGINT COMMENT '分类1',
+    `category2_id` BIGINT COMMENT '分类2',
+    `description` VARCHAR ( 200 ) COMMENT '描述',
+    `cover` VARCHAR ( 200 ) COMMENT '封面',
+    `doc_count` INT COMMENT '文档数',
+    `view_count` INT COMMENT '阅读数',
+    `vote_count` INT COMMENT '点赞数',
+PRIMARY KEY ( `id` )
+) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COMMENT '电子书';
